7f0fdff620f9e46a13dfcfe8724a03ae6fb5aa15
Brayden Langley <brayden167@gmail.com>
2025-03-04 15:12:11 -0800

    Update index.ts

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,7 +2,7 @@ import sendMessage from './sendMessage.js'
 import listMessages from './listMessages.js'
 import acknowledgeMessage from './acknowledgeMessage.js'
 
-// Explicitly type the exported arrays to avoid type inference issues
+// Explicitly type the exported arrays to avoid type inference issues.
 export const preAuth: Array<{ type: string, path: string, func: Function }> = []
 export const postAuth: Array<{ type: string, path: string, func: Function }> = [
   sendMessage,
